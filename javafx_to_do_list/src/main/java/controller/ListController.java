6cef00c59df99f78e0dec2e322bde1ea6b039fce
Rashenshammika <shenwotsan03@gmail.com>
2025-05-18 16:09:41 +0530

    database connect

--- javafx_to_do_list/src/main/java/controller/ListController.java
+++ javafx_to_do_list/src/main/java/controller/ListController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
+import java.sql.*;
+
 public class ListController {
 
     @FXML
@@ -19,6 +21,22 @@ public class ListController {
     @FXML
     void btnOnActionAdd(ActionEvent event) {
 
+    }
+
+    @FXML
+    void btnOnActionReload(ActionEvent event) {
+        try {
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/to_do_list","root","12345");
+            System.out.println(connection);
+
+           Statement statement = connection.createStatement();
+           ResultSet resultSet = statement.executeQuery("SELECT * FROM list");
+
+            System.out.println(resultSet);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
