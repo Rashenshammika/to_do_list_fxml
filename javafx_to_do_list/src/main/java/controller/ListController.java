55ffd2cdf3c45cb93077c0e32ec16516b0ee990c
Rashenshammika <shenwotsan03@gmail.com>
2025-05-18 12:30:57 +0530

    ui update

--- javafx_to_do_list/src/main/java/controller/ListController.java
+++ javafx_to_do_list/src/main/java/controller/ListController.java
@@ -2,9 +2,16 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class ListController {
+
+    @FXML
+    private TableView<?> doneTable;
+
+    @FXML
+    private TableView<?> listTable;
 
     @FXML
     private TextField txtListAdd;
--- javafx_to_do_list/src/main/java/model/ListModel.java
+++ javafx_to_do_list/src/main/java/model/ListModel.java
@@ -1,4 +1,14 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ListModel {
+    private String list;
 }
