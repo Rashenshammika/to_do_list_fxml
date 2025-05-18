ea119142065c0fc31a91e45160db95af8c1460db
Rashenshammika <shenwotsan03@gmail.com>
2025-05-18 11:17:22 +0530

    second commit-model and database connect

--- javafx_to_do_list/src/main/java/controller/ListController.java
+++ javafx_to_do_list/src/main/java/controller/ListController.java
@@ -1,6 +1,17 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class ListController {
+
+    @FXML
+    private TextField txtListAdd;
+
+    @FXML
+    void btnOnActionAdd(ActionEvent event) {
+
+    }
 
 }
